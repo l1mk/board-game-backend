3a80a904b0b50fbeff359661044b720f9b1518ae
l1mk <jhonxlink@hotmail.com>
2020-09-24 20:31:43 -0500

    added exception on render json for character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,7 @@
 class RecordsController < ApplicationController
     def index
         characters = Character.all 
-        render json: characters
+        render json: characters, except: [created_at, :updated_at ]
     end
 
     def create
